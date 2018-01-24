fd0919e14106b50f68d5abb597ac565a84e72c90
Josh Barker <jbarker@t1v.com>
2018-01-24 17:03:28 -0500

    added new parallax function

--- PortfolioLayouts/assets/main.js
+++ PortfolioLayouts/assets/main.js
@@ -1,14 +1,28 @@
-window.onscroll = function() {
+// window.onscroll = function() {
 
-    const element = document.querySelector('.bg-cover');
+//     const element = document.querySelector('.bg-cover');
 
-    var elH = Math.max(element.height, document.scrollingElement.scrollHeight / 2);
-    var winH = window.innerHeight;
-    var scrollTop = document.scrollingElement.scrollTop;
-    var scrollHeight = document.scrollingElement.scrollHeight - winH;
-    var shiftTotal = elH - winH;
-    var scrollPos = scrollTop / scrollHeight;
+//     var elH = Math.max(element.height, document.scrollingElement.scrollHeight / 2);
+//     var winH = window.innerHeight;
+//     var scrollTop = document.scrollingElement.scrollTop;
+//     var scrollHeight = document.scrollingElement.scrollHeight - winH;
+//     var shiftTotal = elH - winH;
+//     var scrollPos = scrollTop / scrollHeight;
     
-    element.style.transform = 'translateY(-' + scrollPos * shiftTotal + 'px)';
+//     element.style.transform = 'translateY(-' + scrollPos * shiftTotal + 'px)';
 
+// }
+
+function goodParallax() {
+    let speed, yPos;
+    let top = window.pageYOffset;
+    const items = document.querySelectorAll('.parallax');
+
+    items.forEach((item) => {
+        speed = item.dataset.speed;
+        yPos = -(top * speed / 100);
+        item.style.transform = `translateY(${yPos}px)`; 
+    });
 }
+
+window.addEventListener('scroll', goodParallax);
